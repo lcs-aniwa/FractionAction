db0047ae7d4f44d9cd64d183e50aecee9cb3be24
Niwa Ayana <aniwa@lcs-mba3092.lcs.on.ca>
2018-04-28 10:24:59 -0400

    Timur, Ayana, and Andrew have done partial solutions.

--- FractionAction/main.swift
+++ FractionAction/main.swift
@@ -1,21 +1,37 @@
-//
-//  main.swift
-//  ChipsFastFoodEmporium
-//
-//  Created by Gordon, Russell on 2018-04-04.
-//  Copyright © 2018 Gordon, Russell. All rights reserved.
-//
-
 import Foundation
 
-// INPUT
-// Collect and filter user input here
+func fractionSimplifier(_ numerator: Int, _ denominator: Int) {
+    var variableForNumerator = numerator
+    var variableForDenominator = denominator
+    if numerator == 0 && denominator != 0{
+        print("your fraction is 0.")
+    } else if denominator == 0 {
+        print("the fraction you have input does not exist. Go to Gordon.")
+    }
+    
+    
+    let remainder1 = variableForNumerator % variableForDenominator
+    let quotient1 = variableForNumerator / variableForDenominator
+    
+    let remainder2 = variableForDenominator % variableForNumerator
+    let quotient2 = variableForDenominator / variableForNumerator
+    
+    
+    if remainder1 == 0 {
+        print(quotient1)
+    } else if remainder2 == 0 {
+        print("1/\(quotient2)")
+    }
+    
+    for i in 1...variableForNumerator {
+        for j in 1...variableForDenominator {
+            if j == i {
+                variableForDenominator /= j
+                variableForNumerator /= i
+            }
+        }
+    }
+   
 
-
-// PROCESS
-// Implement the primary logic of the problem here
-
-
-// OUTPUT
-// Report results to the user here
-
+}
+fractionSimplifier(10, 30)
